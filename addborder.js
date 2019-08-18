a3ce0fe471050e222fb09b9b6849adea4f2c2657
kohalahal <52988648+kohalahal@users.noreply.github.com>
2019-08-18 19:38:10 +0900

    Update addborder.js

--- addborder.js
+++ addborder.js
@@ -38,3 +38,75 @@ function addBorder(img) {
 var ing = new SimpleImage("rodger.png");
 ing = addBorder(ing);
 print(ing);
+
+
+
+/*  혹은
+1.
+
+function addBorder(image, thickness){
+  for (var px of image.values()){
+    var x = px.getX();
+    var y = px.getY();
+    if (x < thickness){
+      px = setBlack(px);
+    }
+    if (x >= image.getWidth()-thickness){
+      px = setBlack(px);
+    }
+    if (y < thickness){
+      px = setBlack(px);
+    }
+    if (y >= image.getHeight()-thickness){
+      px = setBlack(px);
+    }
+  }
+  return image;
+}
+
+2.
+function addBorder(image, thickness){
+    for (var pixel of image.values()){
+        if (pixel.getX() < thickness){
+            pixel = setBlack(pixel);
+        }
+        if (pixel.getX() >= image.getWidth()-thickness){
+            pixel = setBlack(pixel);
+        }
+        if (pixel.getY() < thickness){
+            pixel = setBlack(pixel);
+        }
+        if (pixel.getY() >= image.getHeight()-thickness){
+            pixel = setBlack(pixel);
+        }
+    }
+    return image;
+}
+
+3.
+function pixelOnEdge(image,pixel,horizontalThick, verticalThick){
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (x < verticalThick || x > image.getWidth() - verticalThick){
+        return true;
+    }
+    if (y < horizontalThick || y > image.getHeight() - horizontalThick){
+        return true;
+    }
+    return false;
+}
+
+function addBorders(image,horizontalThick, verticalThick){
+    for (var px of image.values()){
+        if (pixelOnEdge(image,px,horizontalThick,verticalThick)){
+            px = setBlack(px);
+        }
+    }
+    return image;
+}
+
+var img = new SimpleImage("skyline.png");
+img = addBorders(img,40,20);
+print(img);
+
+*/
